d8518b6e63c270189c5872103d2a72ebb700eb4d
ogison <meyo.ogs9@gmail.com>
2024-11-04 00:45:50 +0900

    add suspense追加

--- src/components/wordBookCard.tsx
+++ src/components/wordBookCard.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { Dispatch, SetStateAction } from "react";
+import React, { Dispatch, SetStateAction, Suspense } from "react";
 import { Card, CardContent } from "@/components/ui/card";
 import { getRandomWord } from "@/utils/";
 import { Button } from "./ui/button";
@@ -7,6 +7,7 @@ import { WORD } from "@/types";
 import { Annoyed, Check, Eye, EyeOff, Laugh, Shuffle } from "lucide-react";
 import { Status, Type } from "@/types/enums";
 import { updateStatus } from "@/lib/api";
+import LoadingIcon from "./LoadingIcon";
 
 interface WordBookCardProps {
   currentWord: WORD;
@@ -40,64 +41,70 @@ const WordBookCard: React.FC<WordBookCardProps> = ({
   setError,
 }) => {
   return (
-    <Card className="mb-6 relative">
-      <CardContent className="p-6">
-        {currentWord.status === Status.NotStarted ? (
-          <Annoyed className="mr-2 h-4 w-4" />
-        ) : (
-          <Laugh className="mr-2 h-4 w-4" />
-        )}
-        <h2 className="text-2xl font-bold mb-4 text-center">
-          {currentWord.type === Type.EnglishTranslation
-            ? currentWord.meaning
-            : currentWord.word}
-        </h2>
-        {showMeaning && (
-          <>
-            <p className="text-lg mb-2">
-              <strong>訳:</strong>{" "}
-              {currentWord.type === Type.EnglishTranslation
-                ? currentWord.word
-                : currentWord.meaning}
-            </p>
-            <p className="text-lg italic">
-              <strong>例文:</strong> {currentWord.example}
-            </p>
-          </>
-        )}
-        <div className="flex justify-center space-x-4 mt-6">
-          <Button onClick={() => toggleMeaning(showMeaning, setShowMeaning)}>
-            {showMeaning ? (
-              <EyeOff className="mr-2 h-4 w-4" />
+    <Suspense fallback={<LoadingIcon />}>
+      <Card className="mb-6 relative">
+        <CardContent className="p-6">
+          <Suspense fallback={<LoadingIcon />}>
+            {currentWord.status === Status.NotStarted ? (
+              <Annoyed className="mr-2 h-4 w-4" />
             ) : (
-              <Eye className="mr-2 h-4 w-4" />
+              <Laugh className="mr-2 h-4 w-4" />
             )}
-            {showMeaning ? "訳を隠す" : "訳を表示"}
-          </Button>
-          <Button
-            onClick={() =>
-              updateStatus(
-                currentWord.id,
-                currentWord.status,
-                setWords,
-                setIsFlipping,
-                words,
-                setCurrentWord,
-                setShowMeaning,
-                setError
-              )
-            }
-          >
-            <Check className="mr-2 h-4 w-4" /> 覚えた！
-          </Button>
-          <Button
-            onClick={() => getRandomWord(words, setCurrentWord, setShowMeaning)}
-          >
-            <Shuffle className="mr-2 h-4 w-4" /> 次の単語
-          </Button>
-        </div>
-      </CardContent>
-    </Card>
+          </Suspense>
+          <h2 className="text-2xl font-bold mb-4 text-center">
+            {currentWord.type === Type.EnglishTranslation
+              ? currentWord.meaning
+              : currentWord.word}
+          </h2>
+          {showMeaning && (
+            <>
+              <p className="text-lg mb-2">
+                <strong>訳:</strong>{" "}
+                {currentWord.type === Type.EnglishTranslation
+                  ? currentWord.word
+                  : currentWord.meaning}
+              </p>
+              <p className="text-lg italic">
+                <strong>例文:</strong> {currentWord.example}
+              </p>
+            </>
+          )}
+          <div className="flex justify-center space-x-4 mt-6">
+            <Button onClick={() => toggleMeaning(showMeaning, setShowMeaning)}>
+              {showMeaning ? (
+                <EyeOff className="mr-2 h-4 w-4" />
+              ) : (
+                <Eye className="mr-2 h-4 w-4" />
+              )}
+              {showMeaning ? "訳を隠す" : "訳を表示"}
+            </Button>
+            <Button
+              onClick={() =>
+                updateStatus(
+                  currentWord.id,
+                  currentWord.status,
+                  setWords,
+                  setIsFlipping,
+                  words,
+                  setCurrentWord,
+                  setShowMeaning,
+                  setError
+                )
+              }
+            >
+              <Check className="mr-2 h-4 w-4" /> 覚えた！
+            </Button>
+            <Button
+              onClick={() =>
+                getRandomWord(words, setCurrentWord, setShowMeaning)
+              }
+            >
+              <Shuffle className="mr-2 h-4 w-4" /> 次の単語
+            </Button>
+          </div>
+        </CardContent>
+      </Card>
+    </Suspense>
   );
 };
 
